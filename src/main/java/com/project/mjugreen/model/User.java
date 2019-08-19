ced299881d1e9901f9e95bf867070c15dd60fe34
Aik  Nit <aik.nit2018@gmail.com>
2019-08-19 22:04:22 +0700

    add connect db

---
+++ src/main/java/com/project/mjugreen/model/User.java
@@ -0,0 +1,18 @@
+package com.project.mjugreen.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int Id;
+	private String username;
+	private String password;
+}
